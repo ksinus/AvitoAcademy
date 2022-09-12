9e7081ef423ea490b6a2b8c0b5f27eddacec4fe1
Ksenya Kurinova <68475716+ksinus@users.noreply.github.com>
2022-09-12 13:09:22 +0300

    Update task1.py

--- python/task1.py
+++ python/task1.py
@@ -1,5 +1,8 @@
+'''
+Починить программу (выводить значение скорости и направления ветра в N-ной точке)
+'''
 import requests
 response = requests.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
 points = response.json()
-wind = [series['wind10m']['speed'] for series in points['dataseries']]
+wind = [series['wind10m'] for series in points['dataseries']]
 print(wind)
