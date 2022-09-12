b06a30a37fcd72a664bf0987077481ef54687d3c
Ksenya Kurinova <68475716+ksinus@users.noreply.github.com>
2022-09-12 13:08:24 +0300

    Create task1.py

---
+++ python/task1.py
@@ -0,0 +1,5 @@
+import requests
+response = requests.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
+points = response.json()
+wind = [series['wind10m']['speed'] for series in points['dataseries']]
+print(wind)
