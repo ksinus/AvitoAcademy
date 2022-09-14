1266517ad8d8d31bfe2c4c221caf5fb7b0e174fa
Ksenya Kurinova <68475716+ksinus@users.noreply.github.com>
2022-09-14 23:51:19 +0300

    Update request.py

--- python/task1/request.py
+++ python/task1/request.py
@@ -1,6 +1,3 @@
-'''
-Починить программу (выводить значение скорости и направления ветра в N-ной точке)
-'''
 import requests
 response = requests.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
 points = response.json()
