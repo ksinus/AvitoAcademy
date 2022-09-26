3bc75e13d671149f75dd8ca461b709e30bce3d05
ksenya <ksen.cur@yandex.ru>
2022-09-26 14:49:43 +0300

    Add annotations

--- python/task3/Advert.py
+++ python/task3/Advert.py
@@ -8,7 +8,7 @@ class JsonToObj:
     with access to attributes via a point
     """
 
-    def __init__(self, mapping):
+    def __init__(self, mapping) -> None:
         for key, value in mapping.items():
             if iskeyword(key) is True:
                 key += '_'
@@ -19,31 +19,31 @@ class JsonToObj:
 class ColorizeMixin:
     """ Mixin class for changing color """
 
-    def __init__(self, color_code=35):
+    def __init__(self, color_code=35) -> None:
         self.repr_color_code = color_code
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"\033[1;{self.repr_color_code};40m {self.__repr__()} \033[m"
 
 
 class Advert(ColorizeMixin):
     """ Transform JSON adverts into class objects """
 
-    def __init__(self, mapping):
+    def __init__(self, mapping) -> None:
         super().__init__()
         self.__dict__.update(JsonToObj(mapping).__dict__)
         if self.__dict__.get('price', 0) < 0:
             raise ValueError('Price must be non-negative')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.title} | {self.price} ₽'
 
     @property
-    def price(self):
+    def price(self) -> str:
         return self.__dict__.get('price', 0)
 
     @property
-    def title(self):
+    def title(self) -> str:
         return self.__dict__.get('title')
 
 
@@ -60,13 +60,9 @@ if __name__ == '__main__':
     print(lesson_ad.location.address)
 
     lesson_str = """{
-        "title": "Вельш-корги",
-        "price": 1000,
-        "class": "dogs",
-        "location": {
-            "address": "сельское поселение Ельдигинское"
-    }
+        "title": "Вельш-корги"
+    
     }"""
     lesson = json.loads(lesson_str)
     lesson_ad = Advert(lesson)
-    print(lesson_ad)
+    print(lesson_ad.repr_color_code)
